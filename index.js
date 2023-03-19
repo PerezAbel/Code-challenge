a3de763104c26e07a9ff12c4b95c2119a7d275d6
PerezAbel <126673313+PerezAbel@users.noreply.github.com>
2023-03-19 23:22:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+ function studentsGradeGenerator(grade) {
+if( grade >79 )
+    output = "A";
+
+if( grade <=79 )
+    output = "B" ; 
+
+if( grade >=59 )
+    output = "c";
+
+if( grade >=49 )
+    output = "D";
+
+if( grade <40 )
+    output = "E";
+}
